04c71eaa81dddd8a55f9267f0b1cb98687e6e6f0
KSClopton <kyle.clopton@outlook.com>
2020-06-15 21:04:51 -0400

    Two tests completed. Checks App and Episode components

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Dropdown from "react-dropdown";
 import parse from "html-react-parser";
-
+import {fetchShow} from './API/fetchShow'
 import { formatSeasons } from "./utils/formatSeasons";
 
 import Episodes from "./components/Episodes";
@@ -15,18 +15,13 @@ export default function App() {
   const episodes = seasons[selectedSeason] || [];
 
   useEffect(() => {
-    const fetchShow = () => {
-      axios
-        .get(
-          "https://api.tvmaze.com/singlesearch/shows?q=stranger-things&embed=episodes"
-        )
+    fetchShow()
         .then(res => {
-          setShow(res.data);
-          setSeasons(formatSeasons(res.data._embedded.episodes));
+          console.log(res)
+          setShow(res);
+          setSeasons(formatSeasons(res._embedded.episodes));
         });
-    };
-    fetchShow();
-  }, []);
+    }, []);
 
   const handleSelect = e => {
     setSelectedSeason(e.value);
@@ -42,6 +37,7 @@ export default function App() {
       <h1>{show.name}</h1>
       {parse(show.summary)}
       <Dropdown
+        
         options={Object.keys(seasons)}
         onChange={handleSelect}
         value={selectedSeason || "Select a season"}
